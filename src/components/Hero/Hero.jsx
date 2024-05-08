26202c5cd4cf7a60342da0c6a65c7aa9afd6a027
Ziad Hamdi Saad <130312784+ziad768@users.noreply.github.com>
2024-05-09 00:17:25 +0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,8 +13,8 @@ export const Hero = () => {
         </p>
         <a href="mailto:ziadhamdi7zezo@gmail.com" className={styles.contactBtn}>
           Contact Me
-        </a><a href="https://drive.google.com/file/d/1zLTrkV_vgF9XJqMWN_wdTca_MUBGOnDn/view?usp=drivesdk" download="ZiadHamdiCv.pdf" className={styles.contactBtn} style={{marginTop:"15px"}}>
-          Download CV
+        </a><a href="https://drive.google.com/file/d/1zLTrkV_vgF9XJqMWN_wdTca_MUBGOnDn/view?usp=drivesdk"  className={styles.contactBtn} style={{marginTop:"15px"}}>
+          Go to CV
         </a>
       </div>
       <img
